72244073916135ba501faf244445bd183a2dc592
davidesciaulino <sciaulinodavide@gmail.com>
2025-12-09 20:43:39 +0100

    Created ask_choice func

--- utils.sh
+++ utils.sh
@@ -10,3 +10,26 @@ print_header(){
                                        
 EOF
 }
+
+#Asks the user to choose between two options
+ask_choice(){
+    local category_name=$1
+    local option1_name=$2
+    local option1_pkg=$3
+    local option2_name=$4
+    local option2_pkg=$5
+
+    echo -e "\n[CHOICE] Which $category_name do you want to install?"
+    echo "  1) $option1_name"
+    echo "  2) $option2_name"
+    read -p "Type 1 or 2 (Default 1): " choice
+
+    case $choice in
+        2)
+            echo "$option2_pkg"
+            ;;
+        *)
+            echo "$option1_pkg"
+            ;;
+    esac
+}
