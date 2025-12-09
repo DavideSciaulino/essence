5d0648ea2fc0bfde1ebf0764b54d5f5fa7f9d924
davidesciaulino <sciaulinodavide@gmail.com>
2025-12-09 20:22:21 +0100

    Moved all functions in utils.sh

--- install.sh
+++ install.sh
@@ -10,18 +10,15 @@
 # Essence - Arch Linux Hyprland Bootstrapper
 # ==============================================================================
 
-#Print logo
-print_header(){
-    cat << "EOF"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-     ______                             
-    / ____/____________  ____  ________ 
-   / __/ / ___/ ___/ _ \/ __ \/ ___/ _ \
-  / /___(__  |__  )  __/ / / / /__/  __/        Arch Linux Hyprland Bootstrapper
- /_____/____/____/\___/_/ /_/\___/\___/         losciau
-                                       
-EOF
-}
+#Source utils.sh
+if [[ -f "$SCRIPT_DIR/utils.sh" ]]; then
+    source "$SCRIPT_DIR/utils.sh"
+else
+    echo "Error: utils.sh not found!"
+    exit 1
+fi
     
 clear
 print_header
